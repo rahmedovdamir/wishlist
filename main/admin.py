f9becb71019c9424793a7153ff74c9ecd997ded7
rahmedovdamir <rahmedov17@mail.ru>
2025-10-28 11:42:44 +0300

    first commit and add main list ideas

---
+++ main/admin.py
@@ -0,0 +1,28 @@
+from django.contrib import admin
+from .models import Category,Product,ProductImage, Size, ProductSize
+
+class ProductImageInLine(admin.TabularInline):
+    model = ProductImage
+    extra = 1
+
+class ProductSizeInline(admin.TabularInline):
+    model = ProductSize
+    extra = 1
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'category', 'color' , 'price']
+    list_filter = ['category', 'color']
+    search_fields = ['name', 'color' , 'description']
+    prepopulated_fields = {'slug': ('name',)}
+    inlines = [ProductImageInLine,ProductSizeInline]
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+class SizeAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Size, SizeAdmin)
